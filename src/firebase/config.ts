e1208571e006a79292004c77091a3d355905aa58
Naru Go <naru2go999@gmail.com>
2026-02-14 10:05:27 +0000

    // Import the functions you need from the SDKs you need import { initial

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,9 +1,11 @@
-// TODO: Replace with your actual Firebase project configuration
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 export const firebaseConfig = {
-  apiKey: 'REPLACE_WITH_YOUR_API_KEY',
-  authDomain: 'REPLACE_WITH_YOUR_AUTH_DOMAIN',
-  projectId: 'REPLACE_WITH_YOUR_PROJECT_ID',
-  storageBucket: 'REPLACE_WITH_YOUR_STORAGE_BUCKET',
-  messagingSenderId: 'REPLACE_WITH_YOUR_MESSAGING_SENDER_ID',
-  appId: 'REPLACE_WITH_YOUR_APP_ID',
+  apiKey: "AIzaSyB42YdK2AtI3svUmgSokMt1qqk1I5V3IzE",
+  authDomain: "tracktube-b529c.firebaseapp.com",
+  projectId: "tracktube-b529c",
+  storageBucket: "tracktube-b529c.appspot.com",
+  messagingSenderId: "811200203714",
+  appId: "1:811200203714:web:3962815b6cfacb2360645b",
+  measurementId: "G-M4R8VY7YR9"
 };
